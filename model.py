d058fb6ecaa3200042806837d7037a724ab42a5c
ruungles <rlarson92001@yahoo.com>
2024-11-18 22:38:24 -0800

    Updated model to use VMD and plots of VMD
    Scales data and uses VMD to create frequencies of 10 IFMs

--- model.py
+++ model.py
@@ -1,81 +1,43 @@
+import tensorflow as tf
 import pandas as pd
-#pd.set_option("display.max_columns", None)
-#pd.set_option("display.max_rows", None)
 import numpy as np
 from sktime.transformations.series.vmd import VmdTransformer
-from sktime.datasets import load_solar
+from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
 
+def DataFrameDropcolumns(file, drop_columns):
+    df = pd.read_csv(file)
+    df = df.drop(drop_columns, axis=1)
+    return df
 
-class DataPreprocessor():
-    def __init__ (self, dataFrame):
-        self.dataFrame = dataFrame
-    def getDataFrame (self):
-        return self.dataFrame
-    def setDataFrame (self, dataFrame):
-        self.dataFrame = dataFrame
-    def removeEmptyValues(self):
-        self.dataFrame.dropna(axis=0,inplace=True)
-    
-    # Function calculates the unweighted mean of the previous N closing prices
-    def SimpleMovingAverage(self):
-        self.dataFrame['SMA_20'] = self.dataFrame['Close'].rolling(window=20).mean()
-    
-    # Function calculates the weighted recent prices and responds to quickly changing prices
-    def ExponentialMovingAverage(self):
-        self.dataFrame['EMA_20'] = self.dataFrame['Close'].ewm(span=20, adjust=False).mean()
-    
-    # Function calculates the magnitude of recent prices changes to evalute overbought and oversold conditions
-    # Rs is the average of N up day's gains divided by the average of N down days' losses
-    def RelativeStrengthIndex(self):
-        window_length = 14
-        delta = self.dataFrame['Close'].diff()
-
-        gain = delta.where(delta > 0, 0)
-        loss = -delta.where(delta < 0, 0)
-
-        avg_gain = gain.rolling(window = window_length).mean()
-        avg_loss = loss.rolling(window = window_length).mean()
-
-        rs = avg_gain / avg_loss
-        self.dataFrame['RSI'] = 100 - (100 / (1 + rs))
-    
-    # Function calculates the difference of 2 Exponential Moving Averages(EMAs)
-    # Typical EMAs are 12-day and 26-days
-    def MovingAverageConvergenceDivergence (self):
-        short_ema = self.dataFrame['Close'].ewm(span=12, adjust=False)
-        long_ema = self.dataFrame['Close'].ewm(span=26, adjust=False)
-
-        self.dataFrame['MACD'] = short_ema - long_ema
-        self.dataFrame['Signal_line'] = self.dataFrame['MACD'].ewm(span=9, adjust=False).mean()
-
-    # A middle band (SMA) and an upper and lower band that are typically two standard deviations away from the middle band.
-    def BollingerBands(self):
-        self.dataFrame['STD'] = self.dataFrame['Close'].rolling(window=20).std()
-
-        self.dataFrame['Upper_Band'] = self.dataFrame['SMA_20'] + (2 * self.dataFrame['STD'])
-        self.dataFrame['Lower_Band'] = self.dataFrame['SMA_20'] - (2 * self.dataFrame['STD'])
-def testingVMD():
-    y = load_solar()
-    print(y)
-    transformer = VmdTransformer()
-    modes = transformer.fit_transform(y)
-    print (modes)
 
 def main():
-    testingVMD()
-    """
-    file_location = r"./Data/A.csv"
-    df = pd.read_csv(file_location)
-    preprocessor = DataPreprocessor(df)
-    preprocessor.removeEmptyValues()
-    print(preprocessor.getDataFrame().info())
-    preprocessor.SimpleMovingAverage()
-    newDF = preprocessor.getDataFrame()
-    print(newDF['SMA_20'])
-    preprocessor.ExponentialMovingAverage()
-    newDF2 = preprocessor.getDataFrame()
-    print(newDF2['EMA_20'])
-    print(newDF2.info())
-    """
+    # Driver function of this model. Calls main functions and sets all the values for the 
+    # VMD - BPNN model.
+    scaler = MinMaxScaler()
+    file = r'./Data/AAPL.csv'
+    columns_to_drop = ['Open','High','Low','Volume','Dividends','Stock Splits'] 
+    dataFrame = DataFrameDropcolumns(file, columns_to_drop)
+    dataFrame['Close'] = scaler.fit_transform(dataFrame[['Close']])
+
+    # to test the VMD function, took 1st 50 samples out of the dataset to save on time processing 
+    # and debugging
+    
+    transformer = VmdTransformer(K=10)
+    imfs = transformer.fit_transform(dataFrame['Close'])
+    
+    # plotting the decomposition of the dataset
+    imfs.plot(subplots=True, figsize=(10, 8), title="Intrinsic Mode Functions (IMFs)")
+    plt.suptitle("VMD Decomposition - IMFs", y=0.95)
+    plt.show()
+
+    # TensorFlow BackPropagation Model Building
+    hidden_layer_size = 10
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense
+    ])
+
+
 if __name__ == "__main__":
     main()
