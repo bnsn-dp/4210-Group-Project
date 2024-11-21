c61df1237c4c00031bf2a873a750fb3d374f3a2f
ruungles <rlarson92001@yahoo.com>
2024-11-20 20:47:11 -0800

    Further Adjustments on the model
    Added more graphs and more metrics for evaluation

--- model.py
+++ model.py
@@ -18,21 +18,34 @@ def preprocess_and_decompose(df_training, df_testing, target_column, look_back, 
     transformer = VmdTransformer(K=K)
     imfs_training = transformer.fit_transform(df_training[target_column].values)
     imfs_testing = transformer.transform(df_testing[target_column].values)
+    
+    print("IMFs Training Shape:", imfs_training.shape)
+    print("IMFs Testing Shape:", imfs_testing.shape)
+    print("NaN in IMFs Training:", np.isnan(imfs_training).any())
+    print("Infinity in IMFs Training:", np.isinf(imfs_training).any())
 
     X_train, y_train = create_imf_sequences(imfs_training, look_back)
     X_test, y_test = create_imf_sequences(imfs_testing, look_back)
+    
+    # X_train, y_train = create_imf_sequences(df_training, look_back)
+    # X_test, y_test = create_imf_sequences(df_testing, look_back)
 
     return X_train, y_train, X_test, y_test
+
+def root_mean_squared_error(y_true, y_pred):
+    return tf.sqrt(tf.reduce_mean(tf.square(y_true - y_pred)))
 
 def main():
     # Driver function of this model. Calls main functions and sets all the values for the 
     # VMD - BPNN model.
-    start_date_training  = '2020-08-21 00:00:00-04:00'
-    end_date_training = '2023-08-21 00:00:00-04:00'
-    start_date_testing = '2023-08-22 00:00:00-04:00'
+    print('Model testing and Data Preprocessing starting...')
+
+    start_date_training  = '2016-08-22 00:00:00-04:00'
+    end_date_training = '2023-06-16 00:00:00-04:00'
+    start_date_testing = '2023-06-20 00:00:00-04:00'
     end_date_testing = '2023-09-21 00:00:00-04:00'
-    look_back = 30
-    K = 10
+    look_back = 5
+    K = 5
     target_column = 'Close'
 
 
@@ -41,9 +54,9 @@ def main():
     dataFrame = pd.read_csv(file)
     dataFrame.drop(columns_to_drop, axis=1)
 
-    dataFrame_training = dataFrame[(dataFrame['Date'] >= start_date_training) & (dataFrame['Date'] <= end_date_training)]
-    dataFrame_testing = dataFrame[(dataFrame['Date']>= start_date_testing)&(dataFrame['Date'] <= end_date_testing)]
-
+    dataFrame_training = dataFrame[(dataFrame['Date'] >= start_date_training) & (dataFrame['Date'] <= end_date_training)].copy()
+    dataFrame_testing = dataFrame[(dataFrame['Date']>= start_date_testing)&(dataFrame['Date'] <= end_date_testing)].copy()
+    
     scaler = MinMaxScaler()
     dataFrame_training[target_column] = scaler.fit_transform(dataFrame_training[[target_column]])
     dataFrame_testing[target_column] = scaler.transform(dataFrame_testing[[target_column]])
@@ -51,6 +64,11 @@ def main():
     X_train, y_train, X_test, y_test = preprocess_and_decompose(dataFrame_training, dataFrame_testing, target_column, look_back, K)
     print("Training data shapes:", X_train.shape, y_train.shape)
     print("Testing data shapes:", X_test.shape, y_test.shape)
+    print("NaN in X_test:", np.isnan(X_test).any())
+    print("Infinity in X_test:", np.isinf(X_test).any())
+    print("NaN in y_test:", np.isnan(y_test).any())
+    print("Infinity in y_test:", np.isinf(y_test).any())
+    print("Min y_test:", np.min(y_test), "Max y_test:", np.max(y_test))
 
     # TensorFlow BackPropagation Model Building
     model = tf.keras.Sequential([
@@ -65,17 +83,27 @@ def main():
         tf.keras.layers.Dense(1)
 
     ])
-    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), loss='mse', metrics=['mae'])
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.00001), loss='mse', metrics=['mae', root_mean_squared_error])
     model.summary()
 
-    history = model.fit(X_train,y_train, validation_split = 0.2, epochs= 20, batch_size=32)
+    history = model.fit(X_train,y_train, validation_split = 0.2, epochs= 10, batch_size=10)
 
-    loss, mae = model.evaluate(X_test, y_test)
+    print(history.history['loss'])
+    print(history.history['val_loss'])
+
+    plt.plot(history.history['loss'], label= 'Training Loss')
+    plt.plot(history.history['val_loss'], label = 'Validation Loss')
+    plt.legend()
+    plt.title("Training and Validation Loss")
+    plt.show()
+
+    loss, mae, rmse = model.evaluate(X_test, y_test)
     print('Test loss: ', loss)
     print('Test MAE: ', mae)
+    print('Test RMSE: ', rmse)
 
     y_pred = model.predict(X_test)
-    plt.figure(figsize=(12, 6))
+    plt.figure(figsize=(6, 3))
     plt.plot(y_test, label="Actual")
     plt.plot(y_pred, label="Predicted")
     plt.legend()
